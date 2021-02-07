94f841b89d998310f0dd4d6b6d7bbd73ce62fa78
hrco159753 <hrvojebolesic@hotmail.com>
2021-02-07 19:24:35 +0100

    Change src/include to private/public Create Catainer interface library instead of being executable Create test.h header to be the only header in newly created interface library Create one simple test that tests how testing "framework" works

---
+++ public/test.h
@@ -0,0 +1,12 @@
+//
+// Created by hrvoje on 07. 02. 2021..
+//
+
+#pragma once
+
+#include <iostream>
+
+inline void PrintHelloWorld()
+{
+    std::cout << "Hello world" << std::endl;
+}
---
+++ tests/hello_world_test/main.cpp
@@ -0,0 +1,6 @@
+#include "test.h"
+
+int main() {
+    PrintHelloWorld();
+    return 0;
+}
